0d529956d648f707a2540084135555991039b2af
Lindorm236 <elielbjean@gmail.com>
2023-11-14 16:54:05 +0000

    deuxieme formulaire

---
+++ resultat.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <title>Resultat</title>
+    <link rel="stylesheet" href="styleResult.css">
+</head>
+<form action="">
+    <?php
+    $iMax = $_POST["address"];
+    intval($iMax);
+    for ($i=1; $i <= $iMax; $i++) { 
+       ?>
+       <fieldset>
+        <legend>Adresse <?php echo $i ?></legend>
+       <label for="id">id</label>
+        <input type="text" id="id" name="id">
+        <label for="street">street</label>
+        <input type="text" id="street" name="street">
+        <label for="streetNumber">street Number</label>
+        <input type="text" id="streetNumber" name="streetNumber">
+        <label for="street">street</label>
+        <input type="text" id="street" name="street">
+        <select id="type">
+            <option value="livraison">Livraison</option>
+            <option value="facturation">Facturation</option>
+            <option value="autres">Autres</option>
+        </select>
+        </fieldset>
+       <?php
+    }
+    ?>
+    <a href="index.html"><- Retourner à la page précédente</a>
+</form>
+
+<?php
+
+var_dump($_POST);
+?>
+</html>
