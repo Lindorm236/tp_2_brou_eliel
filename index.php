00d5a7da690aa40291ea145133ecf0fe2b142360
Lindorm236 <elielbjean@gmail.com>
2023-11-14 16:53:35 +0000

    premier formulaire

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="styleIndex.css" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <form action="resultat.php" method="post">
+      <fieldset>
+        <label for="adresse">Entrez le nombre d'adresses svp !</label><br />
+        <input class="saisie" type="text" name="address" id="address" /><br />
+        <button type="submit">Envoyer</button>
+      </fieldset>
+    </form>
+  </body>
+</html>
